6e9ac4e2da05c453b9b2dcd85dc9409960180e61
Hiroyuki Shigeta <shigeta@glgl.org>
2015-09-20 20:10:19 +0900

    add string parser

--- marshal.go
+++ marshal.go
@@ -1,7 +1,26 @@
 package ddb
 
-import "github.com/aws/aws-sdk-go/service/dynamodb"
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
 
-func Marshal(v interface{}) *dynamodb.AttributeValue {
-	return nil
+func Marshal(iv interface{}) map[string]*dynamodb.AttributeValue {
+
+	vv := iv.(map[string]interface{})
+	ret := make(map[string]*dynamodb.AttributeValue)
+
+	for key, value := range vv {
+
+		var attrValue *dynamodb.AttributeValue
+
+		switch v := value.(type) {
+		case string:
+			attrValue = &dynamodb.AttributeValue{S: aws.String(v)}
+		}
+
+		ret[key] = attrValue
+	}
+
+	return ret
 }
--- marshal_test.go
+++ marshal_test.go
@@ -9,13 +9,27 @@ import (
 func TestMarshalMap(t *testing.T) {
 
 	m := map[string]interface{}{
-		"str": "string",
-		"num": 1,
+		"str":   "string",
+		"int":   1,
+		"float": 1.0,
+		"null":  nil,
 	}
 
 	u := ddb.Marshal(m)
+
 	if u == nil {
 		t.Error("t is nil")
+		return
+	}
+
+	if _, ok := u["str"]; !ok {
+		t.Error("not includes element")
+		return
+	}
+
+	str := u["str"].S
+	if *str != "string" {
+		t.Error("str does not match")
 		return
 	}
 }
